1393d02aa3a23282115415df8ec071ec7cec9713
AquarHEAD Lou <aquarhead@ela.build>
2017-10-27 09:50:40 +0000

    Preview of generated function names

--- lib/esi/request_functions.ex
+++ lib/esi/request_functions.ex
@@ -1,14 +1,30 @@
 defmodule ESI.RequestFunctions do
   @moduledoc false
 
-  def generate_function(spec) do
+  def generate_functions(spec) do
+    base_uri = %URI{
+      scheme: spec["schemes"] |> List.first,
+      host: spec["host"],
+      path: spec["basePath"],
+    }
+
+    version_in_path = (base_uri.path == nil)
+
+    Enum.each(spec["paths"], fn {path, v} ->
+      Enum.each(v, fn {method, subspec} ->
+        url_parts = path
+        |> String.trim("/")
+        |> String.split("/")
+        |> remove_version(version_in_path)
+        |> generate_function_name(method)
+
+        IO.puts "#{method}, #{path} => #{ESI.RequestFunctions.generate_function_name(url_parts, method)}"
+      end)
+    end)
   end
 
-  def generate_function_name(http_method, path, version_in_path) do
-    path
-    |> String.split("/") # start with split the path
-    |> Enum.reject(&(&1 == "")) # remove empty things because we start&stop with /
-    |> remove_version(version_in_path) # remove /v1/ etc.. if present
+  def generate_function_name(url_parts, http_method) do
+    url_parts
     |> Enum.reduce(
       [""], # start with "", so we don't have to handle special case for first part
     fn (part, [last_part | rest] = name_parts) ->
--- lib/eve_swagger_interface.ex
+++ lib/eve_swagger_interface.ex
@@ -6,19 +6,6 @@ defmodule EVESwaggerInterface do
   defmacro __using__(opts) do
     resp = Tesla.get("https://esi.tech.ccp.is/_latest/swagger.json?datasource=tranquility")
     spec = Poison.decode!(resp.body)
-    base_uri = %URI{
-      scheme: spec["schemes"] |> List.first,
-      host: spec["host"],
-      path: spec["basePath"],
-    }
-
-    version_in_path = (base_uri.path == nil)
-
-    Enum.each(spec["paths"], fn {path, v} ->
-      Enum.each(v, fn {method, subspec} ->
-        IO.puts "#{method}, #{path} => #{ESI.RequestFunctions.generate_function_name(method, path, version_in_path)}"
-      end)
-    end)
 
     quote do
       use Tesla
